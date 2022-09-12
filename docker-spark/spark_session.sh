c6070d6bb24e5f4b986dcf9f0432a381d036ceab
akhlakm <me@akhlakm.com>
2022-09-12 09:51:32 -0400

    move pyspark env variable to docker entry script

--- docker-spark/spark_session.sh
+++ docker-spark/spark_session.sh
@@ -9,5 +9,9 @@ else
     start-master.sh &
     sleep 5
     start-history-server.sh &
+
+    # jupyter lab
+    export PYSPARK_DRIVER_PYTHON='jupyter-lab'
+    export PYSPARK_DRIVER_PYTHON_OPTS='--allow-root --NotebookApp.token='' --NotebookApp.password='' --no-browser --ip 0.0.0.0 --port=9888'
     pyspark
 fi
