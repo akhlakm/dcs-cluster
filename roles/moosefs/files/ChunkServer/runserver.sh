dc9f22b878c87ae71842cb77d290e18a56c9981e
akhlak <me@akhlakm.com>
2022-09-24 06:12:11 +0000

    remove redundant files

--- roles/moosefs/files/ChunkServer/runserver.sh
+++ roles/moosefs/files/ChunkServer/runserver.sh
@@ -4,11 +4,10 @@ echo "HOSTNAME: $HOSTNAME"
 echo "IPs: $(hostname -I)"
 
 cat /etc_hosts >> /etc/hosts
+cat /etc/hosts
 
 sleep 1
-
 mfschunkserver -f start
 
 echo "Exiting ..!"
 
-tail /etc/hosts
